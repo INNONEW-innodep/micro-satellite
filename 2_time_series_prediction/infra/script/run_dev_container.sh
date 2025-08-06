47999da81de1a213d274be73ab3fb968e9ba6b1c
jaeyu0447 <dbtjdwo0447@gmail.com>
2025-08-06 14:43:41 +0900

    Set docker env and add example code of conv-lstm.

---
+++ 2_time_series_prediction/infra/script/run_dev_container.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+
+PROJECT_ROOT="$(cd "$SCRIPT_DIR/../.." && pwd)"
+
+docker run -it --rm \
+  --name conv-lstm-dev \
+  --gpus all \
+  -v "$PROJECT_ROOT":/workspace \
+  conv-lstm:latest \
+  /bin/bash
